f48cf02cbd8df9e151e05f7d8066633776a517a0
Andres <andresmunozgo@gmail.com>
2020-10-22 04:03:33 -0300

    Commit desafio patron3

---
+++ patron3.rb
@@ -0,0 +1,16 @@
+def patron3(n)
+    mod = 0
+        n.times do |i|
+            mod = i%2
+            if mod == 0
+                print 1
+            else
+                print 2
+            end
+        end
+        puts " "
+    end
+    
+    n = ARGV[0].to_i
+    patron3(n)
+    
